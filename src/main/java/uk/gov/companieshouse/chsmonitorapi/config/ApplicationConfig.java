2f7954cebfa28e0d12221a201a20f08ec1fcf173
dhawes-ch <dhawes@companieshouse.gov.uk>
2024-11-14 12:29:23 +0000

    don't actually need any of it

--- src/main/java/uk/gov/companieshouse/chsmonitorapi/config/ApplicationConfig.java
+++ src/main/java/uk/gov/companieshouse/chsmonitorapi/config/ApplicationConfig.java
@@ -13,74 +13,9 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 @Configuration
 public class ApplicationConfig {
 
-    private final EnvironmentReader environmentReader;
-
-    @Autowired
-    public ApplicationConfig(EnvironmentReader environmentReader) {
-        this.environmentReader = environmentReader;
-    }
-
     @Bean
     public Logger getLogger() {
         return LoggerFactory.getLogger(APPLICATION_NAME_SPACE);
-    }
-
-
-    // Mandatory env vars
-    @Bean
-    public String getBindUrl() {
-        return environmentReader.getMandatoryString("BIND_URL");
-    }
-
-    @Bean
-    public String getCertFile() {
-        return environmentReader.getMandatoryString("CERT_FILE");
-    }
-
-    @Bean
-    public String getApiLocalUrl() {
-        return environmentReader.getMandatoryUrl("API_LOCAL_URL");
-    }
-
-    @Bean
-    public String getChsInternalApiKey() {
-        return environmentReader.getMandatoryString("CHS_INTERNAL_API_KEY");
-    }
-
-    @Bean
-    public String getEricUrl() {
-        return environmentReader.getMandatoryUrl("ERIC_URL");
-    }
-
-    @Bean
-    public String getKeyFile() {
-        return environmentReader.getMandatoryString("KEY_FILE");
-    }
-
-    @Bean
-    public String getMongoUrl() {
-        return environmentReader.getMandatoryUrl("MONG_URL");
-    }
-
-    @Bean
-    public String getMongoDatabase() {
-        return environmentReader.getMandatoryString("MONGO_DATABASE");
-    }
-
-    @Bean
-    public String getMongoCollection() {
-        return environmentReader.getMandatoryString("MONGO_COLLECTION");
-    }
-
-    @Bean
-    public int getMongoTimeout() {
-        return environmentReader.getMandatoryInteger("MONGO_TIMEOUT");
-    }
-
-    //Optional env vars
-    @Bean
-    public Optional<String> getDeveloperUrl() {
-        return Optional.ofNullable(environmentReader.getOptionalUrl("DEVELOPER_URL"));
     }
 
 }
