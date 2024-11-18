1dbfc9280063db1930867efa01f67db2af8e374b
dhawes-ch <dhawes@companieshouse.gov.uk>
2024-11-18 09:42:26 +0000

    tests

--- src/main/java/uk/gov/companieshouse/chsmonitorapi/model/InputSubscription.java
+++ src/main/java/uk/gov/companieshouse/chsmonitorapi/model/InputSubscription.java
@@ -1,10 +1,20 @@
 package uk.gov.companieshouse.chsmonitorapi.model;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * InputSubscription represents the JSON input.
  */
 public class InputSubscription {
 
+    public InputSubscription() {
+    }
+
+    public InputSubscription(String companyNumber) {
+        this.companyNumber = companyNumber;
+    }
+
+    @NotNull
     private String companyNumber;
 
     /**
--- src/test/java/uk/gov/companieshouse/chsmonitorapi/controller/ChsMonitorApiControllerTest.java
+++ src/test/java/uk/gov/companieshouse/chsmonitorapi/controller/ChsMonitorApiControllerTest.java
@@ -2,9 +2,10 @@ package uk.gov.companieshouse.chsmonitorapi.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
@@ -28,11 +30,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.util.UriComponentsBuilder;
+import uk.gov.companieshouse.chsmonitorapi.exception.ServiceException;
+import uk.gov.companieshouse.chsmonitorapi.model.InputSubscription;
 import uk.gov.companieshouse.chsmonitorapi.model.SubscriptionDocument;
 import uk.gov.companieshouse.chsmonitorapi.service.SubscriptionService;
 import uk.gov.companieshouse.logging.Logger;
@@ -48,14 +53,16 @@ class ChsMonitorApiControllerTest {
     private final String COMPANY_NAME = "12345678";
     private final String COMPANY_NUMBER = "TEST_COMPANY";
     private final String USER_ID = "TEST_USER";
+    // If you run this at exactly HH:MM:00 it cuts off the seconds and fails lol
     private final LocalDateTime NOW = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-    private final SubscriptionDocument SUBSCRIPTION = new SubscriptionDocument();
+    private final SubscriptionDocument ACTIVE_SUBSCRIPTION = new SubscriptionDocument();
     private final LocalDateTime CREATED = NOW.minus(Period.ofDays(1))
             .truncatedTo(ChronoUnit.SECONDS);
     private final boolean ACTIVE = true;
+    private SubscriptionDocument INACTIVE_SUBSCRIPTION = new SubscriptionDocument();
     private Page<SubscriptionDocument> SUBSCRIPTIONS;
+    private String EXPECTED_RESPONSE_PAGED;
     private String EXPECTED_RESPONSE;
-    private String RESPONSE_JSON_OBJECT;
 
     @MockBean
     private Logger logger;
@@ -87,16 +94,20 @@ class ChsMonitorApiControllerTest {
 
     @BeforeEach
     void beforeEach() {
-        SUBSCRIPTION.setId(TEST_ID);
-        SUBSCRIPTION.setActive(ACTIVE);
-        SUBSCRIPTION.setCreated(NOW);
-        SUBSCRIPTION.setCompanyName(COMPANY_NAME);
-        SUBSCRIPTION.setCompanyNumber(COMPANY_NUMBER);
-        SUBSCRIPTION.setUserId(USER_ID);
-        SUBSCRIPTION.setCreated(CREATED);
-        SUBSCRIPTIONS = new PageImpl<>(List.of(SUBSCRIPTION, SUBSCRIPTION, SUBSCRIPTION));
+        ACTIVE_SUBSCRIPTION.setId(TEST_ID);
+        ACTIVE_SUBSCRIPTION.setActive(ACTIVE);
+        ACTIVE_SUBSCRIPTION.setCreated(NOW);
+        ACTIVE_SUBSCRIPTION.setCompanyName(COMPANY_NAME);
+        ACTIVE_SUBSCRIPTION.setCompanyNumber(COMPANY_NUMBER);
+        ACTIVE_SUBSCRIPTION.setUserId(USER_ID);
+        ACTIVE_SUBSCRIPTION.setCreated(CREATED);
+        SUBSCRIPTIONS = new PageImpl<>(
+                List.of(ACTIVE_SUBSCRIPTION, ACTIVE_SUBSCRIPTION, ACTIVE_SUBSCRIPTION));
 
-        RESPONSE_JSON_OBJECT = """
+        INACTIVE_SUBSCRIPTION = new SubscriptionDocument(ACTIVE_SUBSCRIPTION);
+        INACTIVE_SUBSCRIPTION.setActive(false);
+
+        EXPECTED_RESPONSE = """
                 {"id":"%s",
                 "userId":"%s",
                 "companyNumber":"%s",
@@ -107,7 +118,7 @@ class ChsMonitorApiControllerTest {
                 "updated":null}
                 """.formatted(TEST_ID, USER_ID, COMPANY_NUMBER, COMPANY_NAME, ACTIVE, CREATED);
 
-        EXPECTED_RESPONSE = """
+        EXPECTED_RESPONSE_PAGED = """
                 {"content":
                 [
                 %s,
@@ -124,7 +135,7 @@ class ChsMonitorApiControllerTest {
                 "numberOfElements":3,
                 "first":true,
                 "empty":false}
-                """.formatted(RESPONSE_JSON_OBJECT, RESPONSE_JSON_OBJECT, RESPONSE_JSON_OBJECT);
+                """.formatted(EXPECTED_RESPONSE, EXPECTED_RESPONSE, EXPECTED_RESPONSE);
     }
 
     @Test
@@ -132,23 +143,23 @@ class ChsMonitorApiControllerTest {
     void shouldReturnListOfSubscriptions() throws Exception {
         when(subscriptionService.getSubscriptions(anyString(), any(Pageable.class))).thenReturn(
                 SUBSCRIPTIONS);
-        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/")
+        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/following")
                 .queryParam("companyNumber", TEST_COMPANY_NUMBER).queryParam("startIndex", 0)
                 .queryParam("itemsPerPage", 10).encode().toUriString();
         mockMvc.perform(get(template)).andDo(print())
-                .andExpectAll(status().isOk(), content().json(EXPECTED_RESPONSE));
+                .andExpectAll(status().isOk(), content().json(EXPECTED_RESPONSE_PAGED));
     }
 
     @Test
     @WithMockUser
     void shouldFailWithNullValues() throws Exception {
-        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/")
+        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/following")
                 .queryParam("startIndex", Optional.empty()).queryParam("itemsPerPage", 10).encode()
                 .toUriString();
         mockMvc.perform(get(template)).andDo(print()).andExpectAll(status().isBadRequest(),
                 status().reason("Required parameter 'startIndex' is not present."));
 
-        template = UriComponentsBuilder.fromHttpUrl("http://localhost/")
+        template = UriComponentsBuilder.fromHttpUrl("http://localhost/following")
                 .queryParam("startIndex", 0).queryParam("itemsPerPage", Optional.empty()).encode()
                 .toUriString();
         mockMvc.perform(get(template)).andDo(print()).andExpectAll(status().isBadRequest(),
@@ -161,10 +172,68 @@ class ChsMonitorApiControllerTest {
         when(subscriptionService.getSubscriptions(anyString(), any(Pageable.class))).thenThrow(
                 new ArrayIndexOutOfBoundsException());
 
-        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/")
+        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/following")
                 .queryParam("startIndex", Integer.MAX_VALUE - 10).queryParam("itemsPerPage", 10)
                 .encode().toUriString();
         mockMvc.perform(get(template)).andDo(print())
                 .andExpect(status().isRequestedRangeNotSatisfiable());
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnSubscription() throws Exception {
+        when(subscriptionService.getSubscription(anyString(), anyString())).thenReturn(
+                ACTIVE_SUBSCRIPTION);
+
+        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/following/1777777")
+                .encode().toUriString();
+        mockMvc.perform(get(template)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(EXPECTED_RESPONSE));
+    }
+
+    @Test
+    @WithMockUser
+    void shouldDeleteSubscription() throws Exception {
+        InputSubscription deletePayload = new InputSubscription("1777777");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(delete("http://localhost/following").with(csrf())
+                .content(objectMapper.writeValueAsString(deletePayload))
+                .contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk());
+
+        when(subscriptionService.getSubscription(anyString(), anyString())).thenThrow(
+                new ServiceException("Not found"));
+
+        String template = UriComponentsBuilder.fromHttpUrl("http://localhost/following/1777777")
+                .encode().toUriString();
+
+        mockMvc.perform(get(template)).andDo(print()).andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    @WithMockUser
+    void shouldFailToDeleteSubscription() throws Exception {
+        InputSubscription deletePayload = new InputSubscription("1777777");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        doThrow(new ServiceException("Service exception")).when(subscriptionService)
+                .deleteSubscription(anyString(), anyString());
+
+        mockMvc.perform(delete("http://localhost/following").with(csrf())
+                        .content(objectMapper.writeValueAsString(deletePayload))
+                        .contentType(MediaType.APPLICATION_JSON)).andDo(print())
+                .andExpect(status().isInternalServerError());
+
+    }
+
+    @Test
+    @WithMockUser
+    void shouldCreateSubscription() throws Exception {
+        InputSubscription createPayload = new InputSubscription("1777777");
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(post("http://localhost/following").with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createPayload)))
+                .andExpect(status().isOk());
     }
 }
